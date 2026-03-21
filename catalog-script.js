1322da3d5d021a9a5445614b99e18080de64163b
carrollton-sudo <c.alx3@proton.me>
2026-03-21 23:39:44 +0200

    Update catalog-script.js

--- catalog-script.js
+++ catalog-script.js
@@ -1,23 +1,25 @@
 /* --- STATE & DATA --- */
 const photoData = {
-    1: "Canon EOS 2000D / EF-S 18-55mm \n ISO1600, 33mm, f/4.5, 1/20s",
-    2: "Canon EOS 2000D / EF-S 18-55mm \n ISO800, 30mm, f/5.6, 1/30s",
-    3: "Canon EOS 2000D / EF-S 18-55mm \n ISO1600, 27mm, f/5.6, 1/30s",
-    4: "Canon EOS 2000D / EF-S 18-55mm \n ISO6400, 50mm, f/5.6, 1/40s",
-    5: "Canon EOS 2000D / EF-S 18-55mm \n ISO3200, 34mm, f/8, 1/200s",
-    6: "Canon EOS 2000D / EF-S 18-55mm \n ISO100, 49mm, f/14, 1/125s",
-    7: "Canon EOS M50 / EF-S 18-55mm \n ISO8000, 53mm, f/8, 1/125s",
-    8: "Canon EOS M50 / EF-S 18-55mm \n ISO8000, 55mm, f/8, 1/125s",
-    9: "Canon EOS M50 / EF-M 15-45mm \n ISO3200, 39mm, f/8, 1/80s",
-    10: "Canon EOS M50 / EF-M 15-45mm \n ISO200, 19mm, f/6.3, 1/125s",
-    11: "Canon EOS M50 / EF-M 15-45mm \n ISO200, 24mm, f/6.3, 1/125s",
-    12: "Canon EOS M50 / EF-M 15-45mm \n ISO3200, 24mm, f/6.3, 1/125s",
-    13: "Canon EOS M50 / EF-M 15-45mm \n ISO3200, 19mm, f/6.3, 1/125s",
-    14: "Canon EOS 2000D / EF-S 18-55mm \n ISO800, 18mm, f/5.6, 1/125s",
-    15: "Canon EOS M50 / EF-M 15-45mm \n ISO800, 27mm, f/4.5, 1/100s",
-    16: "Canon EOS M50 / EF-M 15-45mm \n ISO800, 15mm, f/3.5, 1/100s",
-    17: "Canon EOS M50 / EF-M 15-45mm \n ISO200, 45mm, f/6.3, 1/125s",
-    18: "Canon EOS M50 / EF-M 15-45mm \n ISO2000, 31mm, f/5.6, 1/25s"
+    1: "Canon EOS 2000D / Canon EF-S 18-55mm IS II \n ISO1600, 33mm, f/4.5, 1/20s",
+    2: "Canon EOS 2000D / Canon EF-S 18-55mm IS II \n ISO800, 30mm, f/5.6, 1/30s",
+    3: "Canon EOS 2000D / Canon EF-S 18-55mm IS II \n ISO1600, 27mm, f/5.6, 1/30s",
+    4: "Canon EOS 2000D / Canon EF-S 18-55mm IS II \n ISO6400, 50mm, f/5.6, 1/40s",
+    5: "Canon EOS 2000D / Canon EF-S 18-55mm IS II \n ISO3200, 34mm, f/8, 1/200s",
+    6: "Canon EOS 2000D / Canon EF-S 18-55m IS II \n ISO100, 49mm, f/14, 1/125s",
+    7: "Canon EOS M50 / Canon EF-S 18-55mm IS II \n ISO8000, 53mm, f/8, 1/125s",
+    8: "Canon EOS M50 / Canon EF-S 18-55mm IS II \n ISO8000, 55mm, f/8, 1/125s",
+    9: "Canon EOS M50 / Canon EF-M 15-45mm \n ISO3200, 39mm, f/8, 1/80s",
+    10: "Canon EOS M50 / Canon EF-M 15-45mm IS STM\n ISO200, 19mm, f/6.3, 1/125s",
+    11: "Canon EOS M50 / Canon EF-M 15-45mm IS STM \n ISO200, 24mm, f/6.3, 1/125s",
+    12: "Canon EOS M50 / Canon EF-M 15-45mm IS STM \n ISO3200, 24mm, f/6.3, 1/125s",
+    13: "Canon EOS M50 / Canon EF-M 15-45mm IS STM \n ISO3200, 19mm, f/6.3, 1/125s",
+    14: "Canon EOS 2000D / Canon EF-S 18-55mm IS II \n ISO800, 18mm, f/5.6, 1/125s",
+    15: "Canon EOS M50 / Canon EF-M 15-45mm IS STM \n ISO800, 27mm, f/4.5, 1/100s",
+    16: "Canon EOS M50 / Canon EF-M 15-45mm IS STM \n ISO800, 15mm, f/3.5, 1/100s",
+    17: "Canon EOS M50 / Canon EF-M 15-45mm IS STM \n ISO200, 45mm, f/6.3, 1/125s",
+    18: "Canon EOS M50 / Canon EF-M 15-45mm IS STM \n ISO2000, 31mm, f/5.6, 1/25s",
+    19: "Canon EOS 2000D / Canon EF-S 18-55mm IS II \n ISO250, 29mm, f/5.6, 1/160s",
+    20: "Canon EOS 2000D / Canon EF-S 18-55mm IS II \n ISO3200, 48mm, f/5.6, 1/160s"
 };
 
 let currentAlbum = { start: 0, end: 0, folder: '' };
